d3ce856ab6b1ce7be761ca55e986ac4d35987dde
brunoramux <bruno.lemos@live.com>
2024-02-27 20:19:20 -0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { RouterProvider } from 'react-router-dom'
+import './global.css'
+import { router } from './routes'
+
+export function App() {
+  return (
+    <RouterProvider router={router}/>
+  )
+}   
+
+
