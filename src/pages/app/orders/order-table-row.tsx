5f91e6c679955cf8b42c490cb008989268c2104d
brunoramux <bruno.lemos@live.com>
2024-03-05 21:51:35 -0300

    Order Detail Modal

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,15 +1,22 @@
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { TableCell, TableRow } from "@/components/ui/table";
 import { ArrowRight, Search, X } from "lucide-react";
+import { OrderDetails } from "./order-details";
 
 export function OrderTableRow(){
     return (
         <TableRow>
             <TableCell>
-                <Button variant={"outline"} size="xs">
-                    <Search className="h-3 w-3"/>
-                    <span className="sr-only">Detalhes do pedido</span>   
-                </Button>
+                <Dialog>
+                    <DialogTrigger asChild> 
+                        <Button variant={"outline"} size="xs">
+                            <Search className="h-3 w-3"/>
+                            <span className="sr-only">Detalhes do pedido</span>   
+                        </Button>
+                    </DialogTrigger>
+                    <OrderDetails />
+                </Dialog>
             </TableCell>
             <TableCell className="font-mono text-sm font-medium">0999ighuZkf76</TableCell>
             <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
